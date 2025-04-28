7a1028369c4b38143840440ebd5c77f23c1a6fe1
ern66esto <ernestoleobardo@hotmail.com>
2025-04-28 07:35:53 -0400

    first version

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const Blog = require('./models/blog');
+const logger = require('./utils/logger');
+
+app.use(express.json());
+
+morgan.token('body', (req) => {return req.method === 'POST' ? JSON.stringify(req.body) : '';});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.use(cors());
+
+app.get('/api/blogs', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);  
+  } catch (exception) {
+    next(exception);
+  }
+  
+});
+  
+app.post('/api/blogs', async (request, response, next) => {
+  const blog = new Blog(request.body);
+  try {
+    const result = await blog.save();
+    response.status(201).json(result);
+  } catch (exception) {
+    next(exception);
+  }
+  
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'});
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+  
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'});
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message});
+  }
+  next(error);
+};
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+module.exports = app;
+  
+
---
+++ tests/blog_api.test.js
@@ -0,0 +1,95 @@
+const {test, after, beforeEach} = require('node:test');
+const Blog = require('../models/blog');
+const assert = require('node:assert');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const helper = require('./test_helper');
+
+const api = supertest(app);
+
+//npm test -- --test-only
+//npm test -- --test-name-pattern='the first blog has 7 likes'
+beforeEach(async () => {
+  await Blog.deleteMany({});
+ 
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type',/application\/json/);
+});
+
+test('there are three blogs', async () => {
+  const response = await api.get('/api/blogs');
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+
+test('the first blog has 7 likes', async () => {
+  const response = await api.get('/api/blogs');   
+  
+  const likes = response.body.map(e => e.likes);
+  assert.strictEqual(likes[0], 7);
+});
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10
+  };
+
+  await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type',/application\/json/);
+  const response = await api.get('/api/blogs');
+  const titles = response.body.map(r => r.title);
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1);
+  assert(titles.includes('First class tests'));
+  
+});
+
+test('likes property has default value 0 when is undefined', async () => {
+  const newBlog = {
+    title: 'Ceramic First class',
+    author: 'Ennio C. Mattioli',
+    url: 'https://ceramicstudio.ca/upcoming-courses/'
+  };
+
+  await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type',/application\/json/);
+  const response = await api.get('/api/blogs');
+  const blog = response.body.find(r => r.author === newBlog.author);
+
+  assert(blog.likes === 0);
+});
+
+test('unique identifier name is id', async () => {
+  const response = await api.get('/api/blogs');
+  const propertyName = Object.keys(response.body[0]).find(key => key === 'id');
+
+  assert.strictEqual(propertyName, 'id');
+});
+
+test('response 400 bad request when url or title is missed', async () => {
+  const newBlog = {
+    title: '',
+    author: 'Ennio C. Mattioli',
+    url: 'https://ceramicstudio.ca/upcoming-courses/',
+    likes: 5
+  };
+
+  const response = await api.post('/api/blogs').send(newBlog).expect(400).expect('Content-Type',/application\/json/);
+
+  assert(response.body.error.includes('Blog validation failed'));
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
